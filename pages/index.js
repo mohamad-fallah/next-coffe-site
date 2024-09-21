0e4b7bda2b70610c1904a5b659bb73f06b20e6e9
Mohammadreza Fallahfaal <mfallahfaal@gmail.com>
2024-09-21 10:21:02 +0330

    finish newsLatter sections and use json-server localDB

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,29 @@ import Slider from "@/components/template/Index/Slider";
 import React from "react";
 import About from "@/components/template/Index/About";
 import Services from "@/components/template/Index/Services";
+import Offer from "@/components/template/Index/Offer";
 
-function Index() {
+export default function Index({ data }) {
   return (
     <>
       <Slider />
       <About />
-      <Services />
+      <Services services={data.services}/>
+      <Offer/>
     </>
   );
 }
 
-export default Index;
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:4000/services");
+  const services = await res.json();
+
+  return {
+    props: {
+      data: {
+        services,
+      },
+    },
+    revalidate: 60 * 60 * 12,
+  };
+}
